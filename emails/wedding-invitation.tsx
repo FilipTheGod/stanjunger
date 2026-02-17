fb67bb7aa20188a8667c2344c956aeac1bd0b55f
FilipTheGod <filipstan666@gmail.com>
2026-02-17 17:30:34 -0500

    new message

--- emails/wedding-invitation.tsx
+++ emails/wedding-invitation.tsx
@@ -3,12 +3,8 @@ import {
   Container,
   Head,
   Html,
-  Img,
   Section,
   Text,
-  Link,
-  Row,
-  Column,
 } from "@react-email/components"
 import * as React from "react"
 
@@ -18,61 +14,25 @@ export default function WeddingInvitation() {
       <Head />
       <Body style={main}>
         <Container style={container}>
-          {/* Main Content */}
           <Section style={content}>
-            {/* Invitation PDF/Image */}
-            <Section style={detailsContainer}>
-              <table style={detailsTable} cellPadding="0" cellSpacing="0">
-                <tbody>
-                  <tr>
-                    <td style={detailCellLeft}>
-                      <Text style={heading}>you've got plans :)</Text>
-                      <Text style={paragraph}>
-                        {" "}
-                        we'd love to celebrate our wedding with you! until we
-                        share more details in the upcoming formal invitations,
-                        please see our save the date card below and mark your
-                        calendar.
-                      </Text>
-                      <Section style={invitationSection}>
-                        <Img
-                          src="https://stanjunger.wedding/invitation.jpg"
-                          width="500"
-                          alt="Wedding invitation"
-                          style={invitationImage}
-                        />
-                      </Section>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </Section>
-
-            {/* Website Link */}
-            <Section style={linkSection}>
-              <Text style={paragraph}>our wedding website is up and running!
-                      <br />
-                <Link href="https://stanjunger.wedding" style={linkText}>
-                  stanjunger.wedding
-                </Link>
-                <br />
-              feel free to check back in the coming months for updates and
-              information.
+             <Text style={paragraph}>
+              hi friends,
             </Text>
-            </Section>
+            <Text style={paragraph}>
+              we're becoming increasingly aware that, due to the unpredictable timing of our legal immigration process, there is a chance we may not be able to travel as planned by the time of our wedding. at this point, we're not making any changes to our date, but we ask that you hold off on booking flights or making firm travel plans until late spring, when we hope to have more clarity.
+            </Text>
 
-            {/* Footer with Date and Calendar */}
-            <Section style={footerSection}>
-              <Text style={footerDate}>saturday, august 29, 2026
-                <br />
-                 <Link
-                  href="https://calendar.google.com/calendar/render?action=TEMPLATE&text=stan-junger+wedding&dates=20260829/20260830&&location=Prod%2C+Romania"
-                  style={linkText}
-                >
-                  add to calendar
-                </Link>
-              </Text>
-            </Section>
+            <Text style={paragraph}>
+              we know many of you would be traveling a long way for this, and we're truly sorry to ask you to wait. the delays we're facing are outside of our control, but celebrating with you means the world to us, so we want to make sure we can do that without uncertainty hanging over it.
+            </Text>
+
+            <Text style={paragraph}>
+              we promise to keep you updated and will share more news as soon as we can.
+            </Text>
+
+            <Text style={signature}>
+              – ines & filip
+            </Text>
           </Section>
         </Container>
       </Body>
@@ -88,81 +48,28 @@ const main = {
 
 const container = {
   margin: "0 auto",
-  padding: "0px 20px",
+  padding: "40px 20px",
   maxWidth: "600px",
 }
 
 const content = {
-  textAlign: "center" as const,
-}
-
-const heading = {
- fontSize: "16px",
-  fontWeight: "200",
-  color: "#000000",
-  marginBottom: "50px",
-  textTransform: "lowercase" as const,
+  textAlign: "left" as const,
 }
 
 const paragraph = {
   fontSize: "16px",
-  fontWeight: "200",
+  fontWeight: "300",
   color: "#000000",
-  lineHeight: "1.6",
-  marginBottom: "20px",
+  lineHeight: "1.7",
+  marginBottom: "24px",
   textTransform: "lowercase" as const,
 }
 
-const invitationSection = {
-  margin: "10px 0",
-  marginTop: "60px",
-  textAlign: "center" as const,
-}
-
-const invitationImage = {
-  margin: "0 auto",
-  maxWidth: "100%",
-  height: "auto",
-  border: "1px solid #e5e7eb",
-}
-
-const detailsContainer = {
-  marginTop: "20px",
-  marginBottom: "20px",
-}
-
-const detailsTable = {
-  width: "100%",
-}
-
-const detailCellLeft = {
-  verticalAlign: "top" as const,
-  width: "50%",
-  textAlign: "center" as const,
-}
-
-const linkSection = {
-  marginTop: "40px",
-  marginBottom: "40px",
-}
-
-const linkText = {
-  fontSize: "14px",
-  fontWeight: "200",
-  color: "#6b7280",
-  textTransform: "lowercase" as const,
-  textDecoration: "underline",
-}
-
-
-const footerSection = {
-  marginTop: "30px",
-  textAlign: "center" as const,
-}
-
-const footerDate = {
+const signature = {
   fontSize: "16px",
-  fontWeight: "200",
-  color: "#6b7280",
-
+  fontWeight: "300",
+  color: "#000000",
+  lineHeight: "1.7",
+  marginTop: "32px",
+  textTransform: "lowercase" as const,
 }
